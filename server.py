38d11aaebb50824744ff33b59606328017890cc1
Julia Guenther <juliag1@alumni.stanford.edu>
2016-01-24 18:47:32 -0800

    All parts of website visible in browser

--- server.py
+++ server.py
@@ -6,15 +6,11 @@ from flask import Flask, render_template, redirect, request, flash, session
 app = Flask(__name__)
 app.secret_key = "HyrVFG2jcVlpN0qH"
 
-@app.route('/', methods=["POST","GET"])
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/', methods=["GET"])
 def index():
-    """
-    Renders the main page, index.html, from which all routes are 
-    served using Angular.js
-    """
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.debug = True
-
-    app.run()
+    app.run(debug = True)
