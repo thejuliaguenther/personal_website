5d21ae86d2bdcbdcb39dae6cc90f8c51c04199cb
Julia Guenther <juliag1@alumni.stanford.edu>
2016-01-25 09:29:04 -0800

    Preparing to deploy

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+import os
 
 from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, redirect, request, flash, session
 
 app = Flask(__name__)
-app.secret_key = "HyrVFG2jcVlpN0qH"
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
 
 app.jinja_env.undefined = StrictUndefined
 
@@ -13,4 +14,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
+
+    
