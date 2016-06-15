d2aafa0634d8a74152c0762b054522eb2dbbff5a
Julia Guenther <juliag1@alumni.stanford.edu>
2016-06-14 17:34:56 -0700

    Cleaning up

--- static/js/projectsApp.js
+++ static/js/projectsApp.js
@@ -18,37 +18,3 @@ projectsApp.config(['$routeProvider', function ($routeProvider) {
         
 }]);
 
-// var projectsApp = angular.module('projectsApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ui.router']);
-
-// projectsApp.config(function($stateProvider, $urlRouterProvider) {
-//     $urlRouterProvider.otherwise('/tab/dash');
-//         $stateProvider
-//         .state('view1', {
-//             url: '/',
-//             templateUrl: 'static/pages/support-genius.html'
-//         })
-//         .state('view2', {
-//             url: '/ratings',
-//             templateUrl: 'static/pages/ratings-analysis.html'
-//         });
-//     })
-//     .controller('projectCtrl', function($scope, $location, $log) {
-//         $scope.selectedIndex = 0;
-
-//         $scope.$watch('selectedIndex', function(current, old) {
-//             switch (current) {
-//                 case 0:
-//                     $location.url("/");
-//                     break;
-//                 case 1:
-//                     $location.url("/ratings");
-//                     break;
-//             }
-//         });
-//     });
-
-
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
-**/
