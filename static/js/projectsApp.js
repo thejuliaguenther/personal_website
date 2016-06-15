93e9dc2d99a03344c4d96dacd98637f008578199
Julia Guenther <juliag1@alumni.stanford.edu>
2016-06-14 17:33:51 -0700

    Angular tabs working

--- static/js/projectsApp.js
+++ static/js/projectsApp.js
@@ -1,48 +1,51 @@
-// var projectsApp = angular.module('projectsApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngRoute']);
+var projectsApp = angular.module('projectsApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngRoute']);
 
-// projectsApp.config(['$routeProvider', function ($routeProvider) {
+projectsApp.config(['$routeProvider', function ($routeProvider) {
        
-//     $routeProvider
+    $routeProvider
         
-//     .when('/', {
-//         templateUrl: 'static/pages/support-genius.html'
-//         })
-
-//     .when('/ratings', {
-//         templateUrl: 'static/pages/ratings-analysis.html'
-//     })
-        
-        
-// }]);
-
-var projectsApp = angular.module('projectsApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ui.router']);
-
-projectsApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/tab/dash');
-        $stateProvider
-        .state('view1', {
-            url: '/',
-            templateUrl: 'static/pages/support-genius.html'
+    .when('/', {
+        templateUrl: 'static/pages/support-genius.html'
         })
-        .state('view2', {
-            url: '/ratings',
-            templateUrl: 'static/pages/ratings-analysis.html'
-        });
-    })
-    .controller('projectCtrl', function($scope, $location, $log) {
-        $scope.selectedIndex = 0;
 
-        $scope.$watch('selectedIndex', function(current, old) {
-            switch (current) {
-                case 0:
-                    $location.url("/");
-                    break;
-                case 1:
-                    $location.url("/ratings");
-                    break;
-            }
-        });
+    .when('/ratings', {
+        templateUrl: 'static/pages/ratings-analysis.html'
+    })
+    .otherwise({
+        redirectTo: '/'
     });
+        
+        
+}]);
+
+// var projectsApp = angular.module('projectsApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ui.router']);
+
+// projectsApp.config(function($stateProvider, $urlRouterProvider) {
+//     $urlRouterProvider.otherwise('/tab/dash');
+//         $stateProvider
+//         .state('view1', {
+//             url: '/',
+//             templateUrl: 'static/pages/support-genius.html'
+//         })
+//         .state('view2', {
+//             url: '/ratings',
+//             templateUrl: 'static/pages/ratings-analysis.html'
+//         });
+//     })
+//     .controller('projectCtrl', function($scope, $location, $log) {
+//         $scope.selectedIndex = 0;
+
+//         $scope.$watch('selectedIndex', function(current, old) {
+//             switch (current) {
+//                 case 0:
+//                     $location.url("/");
+//                     break;
+//                 case 1:
+//                     $location.url("/ratings");
+//                     break;
+//             }
+//         });
+//     });
 
 
 /**
